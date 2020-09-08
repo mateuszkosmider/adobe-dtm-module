df0d5670671ab2137960c5953d1bc7d0a17b4f42
Mateusz Kosmider <mateusz.kosmider@fourthwall.com>
2020-09-09 00:22:33 +0200

    Cleanup

--- src/Snippet.ts
+++ src/Snippet.ts
@@ -1,17 +1,3 @@
-// export const Snippets: ISnippet = {
-//   script: function (url) {
-//     const script = document.createElement('script')
-//     script.type = 'text/javascript'
-//     script.async = true
-//     script.src = url
-//     return script
-//   },
-
-//   digitalData: function (digitalData, digitalDataName) {
-//     return `var ${digitalDataName} = ${JSON.stringify(digitalData)}`
-//   },
-// }
-
 const script = (url: string) => {
   const script = document.createElement('script')
   script.type = 'text/javascript'
--- src/types.ts
+++ src/types.ts
@@ -3,19 +3,3 @@ export interface IData {
   digitalData: object
   digitalDataName?: string
 }
-
-// export interface ITags {
-//   script: HTMLScriptElement
-//   digitalDataVar: string
-// }
-
-export interface ISnippet {
-  script: (url: string) => HTMLScriptElement
-  digitalData: (digitalData: object, digitalDataName: string) => string
-}
-
-export interface IManager {
-  initialize: (params: IData) => void
-  dtm: (args: IData) => any
-  insertDataScript: (digitalData: string) => HTMLScriptElement
-}
